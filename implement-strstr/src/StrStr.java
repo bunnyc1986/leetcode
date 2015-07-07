e427f2f798141a74c3c1457587e2732b72f7e54b
Yang Fan <bunnyc1986@gmail.com>
2015-07-07 00:26:41 -0700

    implement-strstr

---
+++ implement-strstr/src/StrStr.java
@@ -0,0 +1,23 @@
+/**
+ * https://leetcode.com/problems/implement-strstr/
+ */
+public class StrStr {
+
+    public int strStr(String haystack, String needle) {
+        for(int i=0; i<=haystack.length() - needle.length();i++) {
+            String sub = haystack.substring(i, i+needle.length());
+            if (sub.equals(needle)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        StrStr strStr = new StrStr();
+        String haystack = "aaaba";
+        String needle = "ba";
+        System.out.println(strStr.strStr(haystack, needle));
+
+    }
+}
