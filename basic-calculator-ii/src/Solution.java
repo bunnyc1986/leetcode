53d5a0179bd886ef33b970b5938e7440d7482f6d
Yang Fan <bunnyc1986@gmail.com>
2015-07-09 01:05:53 -0700

    basic-calculator-ii

--- basic-calculator-ii/src/Solution.java
+++ basic-calculator-ii/src/Solution.java
@@ -1,5 +1,52 @@
+import java.util.Stack;
+
 /**
- * Created by yangfan on 7/9/15.
+ * https://leetcode.com/problems/basic-calculator-ii/
  */
 public class Solution {
+    static final char ZERO = '0';
+
+    public int calculate(String s) {
+        Stack<Integer> stack = new Stack<Integer>();
+        int num = 0;
+        char sign = '+';
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                num = num * 10 + s.charAt(i) - ZERO;
+            }
+            if (!Character.isDigit(s.charAt(i)) && !Character.isSpaceChar(s.charAt(i))
+                    || i == s.length() - 1) {
+                switch (sign) {
+                    case '+':
+                        stack.push(num);
+                        break;
+                    case '-':
+                        stack.push(0 - num);
+                        break;
+                    case '*':
+                        stack.push(stack.pop() * num);
+                        break;
+                    case '/':
+                        stack.push(stack.pop() / num);
+                        break;
+                    default:
+                }
+                sign = s.charAt(i);
+                num = 0;
+            }
+        }
+        int sum = 0;
+        for (int e : stack) {
+            sum += e;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().calculate("3+2*2"));
+        System.out.println(new Solution().calculate(" 3/2 "));
+        System.out.println(new Solution().calculate(" 3+5 / 2 "));
+        System.out.println(new Solution().calculate("42"));
+    }
 }
