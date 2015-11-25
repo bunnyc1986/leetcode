62094485b9832d15822a25c33f6e9adbf7df6558
Yang Fan <bunnyc1986@gmail.com>
2015-11-24 17:01:29 -0800

    Wiggle sort, fix a mistake

--- wiggle-sort/src/Solution.java
+++ wiggle-sort/src/Solution.java
@@ -8,7 +8,7 @@ import java.util.Arrays;
 public class Solution {
     public void wiggleSort(int[] nums) {
         for(int i=1; i<nums.length; i++) {
-            if ((i%2==0) != (nums[i]) < nums[i-1]) {
+            if ((i%2==0) != (nums[i] < nums[i-1])) {
                 int t = nums[i];
                 nums[i] = nums[i-1];
                 nums[i-1] = t;
